7743136c595842add4877c864687e3cfe788ec94
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-22 16:42:15 +0100

    Add button clicking func.

--- keyboard.py
+++ keyboard.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from functools import partial
 
 
 class Main_Application(tk.Frame):
@@ -18,24 +19,31 @@ class Main_Application(tk.Frame):
         self.master.resizable(0, 0)
         self.master.config(bg="sky blue")
         self.text_box = tk.Text(master, width=90, height=10, wrap=tk.WORD)
-        self.text_box.grid(row=0, columnspan=40)
+        self.text_box.grid(row=0, column=0, columnspan=40, padx=10, pady=10)
 
         var_row = 4
         var_col = 0
         for button in self.BUTTONS:
 
+            # partial takes care of function and argument
+            cmd = partial(self.button_command, button)
+
             if button != "Space":
-                tk.Button(master, text=button, width=8, bg="black",
-                          fg="white", command=None).grid(row=var_row, column=var_col)
+                tk.Button(master, text=button, width=8, bg="black", fg="white",
+                          command=cmd).grid(row=var_row, column=var_col)
             else:
-                tk.Button(master, text=button, width=40, bg="black", fg="white", command=None).grid(
+                tk.Button(master, text=button, width=40, bg="black", fg="white", command=cmd).grid(
                     columnspan=40, row=var_row, column=var_col)
+
+            if var_col > 14:
+                var_col = 0
+                var_row = var_row + 1
 
             var_col = var_col + 1
 
-            if var_col % 15 == 0:
-                var_col = 0
-                var_row = var_row + 1
+    def button_command(self, button):
+        print("What Iam pressed?: " + button)
+        return
 
 
 root = tk.Tk()
